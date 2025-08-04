8b3ed77450ffe927abb9b018cf45339bcb33d7ff
hangocdinhnew <dinhndrwnew@gmail.com>
2025-08-04 13:52:42 +0700

    Rename.

--- myonruntime/src/MyonRuntime/Core/Log.cpp
+++ myonruntime/src/MyonRuntime/Core/Log.cpp
@@ -18,7 +18,7 @@ void Log::Init() {
   logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 
   s_CoreLogger =
-      std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+      std::make_shared<spdlog::logger>("MYON", begin(logSinks), end(logSinks));
   spdlog::register_logger(s_CoreLogger);
   s_CoreLogger->set_level(spdlog::level::trace);
   s_CoreLogger->flush_on(spdlog::level::trace);
